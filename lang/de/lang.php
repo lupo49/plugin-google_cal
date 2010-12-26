54c8d595d95ba89bec51f153e186086995e404f8
Matthias Schulte <post@lupo49.de>
2010-12-26 12:22:06 +0100

    added german language

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     kite <Kite@puzzlers.org>
+ */
+
+$lang['gcal_Bad_calendar'] = 'Ungültige Google Calendar Anfrage.';
+$lang['gcal_No_JS'] = 'Das Wiki kann diesen Kalendar nicht anzeigen.';
+$lang['gcal_Invalid_mode'] = 'Fehler im Google Calendar-Plugin.';
+
+?>
---
+++ lang/de/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Christopher Smith <chris@jalakai.co.uk>
+ */
+ 
+// for the configuration manager
+$lang['gcal_js_ok']  = 'JavaScript URLs erlauben';
+
+?>
